402de96611379d7143e2ec42c85973414fc2fcf4
Farhan Akhtar <farhanakhtar358@gmail.com>
2025-03-15 00:34:24 +0530

    User I/O String Program.

--- Basic_C#_Programs/02_user_input_as_string.cs
+++ Basic_C#_Programs/02_user_input_as_string.cs
@@ -1,1 +1,12 @@
 // C# file generated automatically
+using System;
+
+public class IO 
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"My name is {name}");
+    }
+}
