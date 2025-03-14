1a8b0332f18a6747ee7ddd08f0cdeede1985cb46
Farhan Akhtar <farhanakhtar358@gmail.com>
2025-03-15 01:12:04 +0530

    If Condition

---
+++ Basic_C#_Programs/04_if.cs
@@ -0,0 +1,17 @@
+// C# file generated automatically
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        double pinCode = 530045;
+        
+        if (pinCode == 570045)
+        {
+            Console.WriteLine("Yes this is your Pincode.");
+        }
+        
+        Console.WriteLine("No this is not your Pincode.");
+    }
+}
